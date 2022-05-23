4b2bc5305a1caf81e321100af44e31498fcaaa09
Margus Porkveli <margus.porkveli@gmail.com>
2022-05-23 08:58:18 +0300

    Commit

--- src/main/Java/com/LHV/nameMatching/Person.java
+++ src/main/Java/com/LHV/nameMatching/Person.java
@@ -1,7 +1,6 @@
 package com.LHV.nameMatching;
 
 public class Person{
-    //int code;
     public
     String name;
     public int score;
@@ -10,11 +9,6 @@ public class Person{
         this.name = name;
         this.score = score;
     }
-
-    /*public void setScore(int code){
-        this.code = code;
-    }*/
-
     public void setName(String name){
         this.name = name;
     }
@@ -23,10 +17,6 @@ public class Person{
         this.score = score;
     }
 
-    /*public int getCode(){
-        return code;
-    }*/
-
     public String getName(){
         return name;
     }
@@ -34,11 +24,6 @@ public class Person{
     public int getScore(){
         return score;
     }
-/*
-    @Override
-    public int compare(Person p1, Person p2) {
-        return p1.getScore().compareTo(p2.getScore());
-    }*/
 
     public String toString() {
         return "name= " + name + "; score= " + score+"%";
